c311d1ad2cbcb5336f35951b98a6e0f6d541ff87
vamshidhar <bandojuvamshi143@gmail.com>
2018-02-07 23:52:38 +0530

    first commit

---
+++ src/main/java/demo1/sum.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class sum {
+	public static void main(String[] args) 
+	{
+		int a=1,b=2,c;
+		c=a+b;
+		System.out.println(+c);
+	}
+	
+}
---
+++ src/test/java/demo1/samtest.java
@@ -0,0 +1,14 @@
+package demo1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class samtest {
+
+	
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
